148662e03b11891eb88a1db00799b4ca9dce6029
exam <exam@len15.mec.ac.in>
2019-10-10 14:49:44 +0800

    Initialised eclosure

---
+++ eclosures.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+class nfa
+{
+	private:
+		int a_states;
+		int a_symbols;
+		std::vector<std::unordered_map<int,int>> transitions;
+	public:
+		nfa(int s, int syms);
+
+		void ip_trans(int);
+};
+
+int main()
+{
+	int n, t, syms;
+
+	std::cout << "Enter number of alphabets: ";
+	std::cin >> syms;
+	std::cout << "Enter number of states: ";
+	std::cin >> n;
+	std::cout << "Enter number of transitions: ";
+	std::cin >> t;
+
+	nfa mynfa(n, syms);
+	mynfa.ip_trans(t);
+
+	return 0;
+}
+
+nfa::nfa(int s, int syms)
+{
+	a_states = s;
+	a_symbols = syms;
+
+	transitions.resize(syms);
+}
+
+void nfa::ip_trans(int t)
+{
+	for(int i=0;i<t;i++)
+	{
+		int t1, t2, in;
+		std::cin >> t1 >> in >> t2;
+
+		
+	}
+}
